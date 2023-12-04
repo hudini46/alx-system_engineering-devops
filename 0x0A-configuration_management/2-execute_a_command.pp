aa060aba8e049eee2c385cd9eaccd9af5ad9e0bc
hudini46 <owenkiilu37@gmail.com>
2023-12-04 09:52:10 -0800

    vanz

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+
+# Kill the process named killmenow
+exec { 'killmenow':
+  # Use the pkill command
+  command => 'pkill killmenow',
+  # Only run the command if the process exists
+  onlyif  => 'pgrep killmenow',
+  # Set the path for the command to execute
+  path    => '/bin:/usr/bin',
+}
