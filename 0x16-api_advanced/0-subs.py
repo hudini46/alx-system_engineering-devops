ca6e9c12173998146305dcfcdf44ad4890d6854d
hudini46 <owenkiilu37@gmuil.com>
2024-08-14 20:28:24 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 """
-0-subs
+Contains a function that works with Reddit API
 """
+import json
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'custom_agent'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        subscribers = data.get('subscribers', 0)
-        return subscribers
-    else:
+    """function that takes a subredit and returns number of subs"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
         return 0
-
-if __name__ == "__main__":
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    json_data = data.json()
+    results = json_data.get("data").get("subscribers")
+    return results
