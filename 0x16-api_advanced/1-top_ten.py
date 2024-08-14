a4b1093054f1cbc54df0a5de9a787b68fad8eab7
hudini46 <owenkiilu37@gmuil.com>
2024-08-14 20:37:11 +0000

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """
-1-top_ten
+Queries Reddit API.
+Prints the titles of first 10 hot posts for a gven subreddit.
 """
+import json
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'custom_agent'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post.get('data', {}).get('title'))
-    else:
-        print(None)
-
-if __name__ == "__main__":
-    top_ten = __import__('1-top_ten').top_ten
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    """gets the top 10 post of a subreddit"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+        return
+    json_data = data.json().get("data").get("children", [])
+    if len(json_data) == 0:
+        print("None")
+        return
+    for post in json_data[0:10]:
+        print(post.get("data").get("title"))
